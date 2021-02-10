588c8b7f652ce5de4ee9f15a15e39c750055297f
Pranav Doshetty <pranav.doshetty@gmail.com>
2021-02-10 16:49:36 +0530

    fs mod

--- app.js
+++ app.js
@@ -33,3 +33,21 @@ console.log(`Free Memory: ${os.freemem()}`);
 console.log('\n');
 
 
+//fs module
+const fs = require('fs');
+fs.readdir('./',function(err, files){
+	if(err) console.log('Error: '+err);
+	else console.log(files);
+})
+
+
+
+//event module
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', function(){
+	console.log('Listener is called');
+});
+
+emitter.emit('messageLogged');
