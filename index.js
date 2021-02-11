dbc7ef1e1f65e8cee46e58b08d35d6068e6ffd7d
Pranav Doshetty <pranav.doshetty@gmail.com>
2021-02-11 12:52:33 +0530

    express-post-req

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app=express();
 
+app.use(express.json());//middleware 
+
 const courses = [
     {id:1, name:'math'},
     {id:2, name:'science'},
@@ -23,6 +25,16 @@ app.get('api/doj/:dd/:mm/:yyyy', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     let course = courses.find(c => c.id === parseInt(req.params.id));
     if(!course) res.status(404);
+    res.send(course);
+});
+
+//POST requests
+app.post('api/courses', (req, res) => {
+    let course = {
+        id: courses.length + 1, 
+        name: req.body.name
+    };
+    courses.push(course);
     res.send(course);
 });
 
